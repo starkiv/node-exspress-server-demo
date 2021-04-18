e70f917662b8cbbf070e1477ef18b09b25cf748f
starkiv <yuriistarkiv@gmail.com>
2021-04-18 22:00:22 +0100

    Create parseResponse.js

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import home from './home';
 import info from './info';
 import errorHandler from './errorHandler';
 import logger from './logger';
+import parseResponse from './parseResponse';
 
 const app = express();
 const port = 5000;
 
 logger(app);
+parseResponse(app);
 app.get('/', home);
 app.post('/info', info);
 errorHandler(app);
--- src/info.js
+++ src/info.js
@@ -1,6 +1,6 @@
 export default function info(req, res) {
-  const a = 123;
-  const b = Math.random();
-  const c = a + b;
-  res.status(200).json('Info HERE!!!' + c);
+  const a = req.body.a;
+  const b = req.body.b;
+  const sum = a + b;
+  res.status(200).json('Info HERE!!!' + sum);
 }
